8e4e778ad723c203a1d44896836c66238ba64d78
adenya-arn <aadenya7@gmail.com>
2026-06-26 13:01:42 +0300

    Initial commit

---
+++ src/data/projects.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "File Uploader",
+    description:
+      "Cloud-style file management application with authentication, folders, and file organization.",
+    tech: ["Node.js", "Express", "PostgreSQL", "Prisma"],
+    github: "#",
+    demo: "#",
+  },
+
+  {
+    title: "Blog API",
+    description:
+      "RESTful API with authentication, authorization, and content management.",
+    tech: ["Express", "PostgreSQL", "Prisma"],
+    github: "#",
+    demo: "#",
+  },
+
+  {
+    title: "Blog Client",
+    description:
+      "Frontend application consuming a blog API and displaying content dynamically.",
+    tech: ["React", "JavaScript", "CSS"],
+    github: "#",
+    demo: "#",
+  },
+
+  //   {
+  //     title: "Where's Waldo",
+  //     description:
+  //       "Interactive photo tagging game inspired by the classic Where's Waldo challenge.",
+  //     tech: ["React", "Node.js", "PostgreSQL"],
+  //     github: "#",
+  //     demo: "#",
+  //   },
+];
